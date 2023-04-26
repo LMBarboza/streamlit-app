26aebd27e0e8b2278192da093e5f330a33254e1a
LMBarboza <leon.uchoa@gmail.com>
2023-04-25 21:48:34 -0300

    first commit

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-def main() -> None:
-    pass
+#import data and write streamlit app
+@st.cache
+def load_data():
+    df = pd.read_csv('data/MunicipalDebtAnalysis.csv')
+    return df
 
-if __name__ == '__main__':
-    main()
+df = load_data()
+
+st.title('Análise de Dívida Municipal')
+st.write(df)
+
+
+st.line_chart(df)
+
+st.map(df)
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c'])
+    st.line_chart(chart_data)
